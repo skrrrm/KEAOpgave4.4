3ed474d2eed9beb7ba71b39d0a2cb82edbf3435b
skrrrm <a@macbook-windows>
2020-03-30 09:20:45 +0200

    Add project files.

---
+++ KEAOpgave4.4/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace KEAOpgave4._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox43 = new System.Windows.Forms.ListBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox43
+            // 
+            this.listBox43.FormattingEnabled = true;
+            this.listBox43.ItemHeight = 25;
+            this.listBox43.Location = new System.Drawing.Point(100, 85);
+            this.listBox43.Name = "listBox43";
+            this.listBox43.Size = new System.Drawing.Size(302, 604);
+            this.listBox43.TabIndex = 0;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(408, 85);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(285, 75);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Click the button\r\nto get numbers listed\r\nvia the formula: [ 43 % x =0 ]";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(408, 620);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 25);
+            this.labelResult.TabIndex = 2;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(413, 211);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(123, 47);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 777);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.listBox43);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox43;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ KEAOpgave4.4/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave4._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Variables
+            int counter = 0;
+            
+            listBox43.Items.Clear(); // Clears box on each click
+
+            for (int i = 1; i <= 1000000; i++) // while below or equal to 1000000
+            {
+                if (i % 43 == 0) // we find every number that is true to the if statment
+                {
+                    listBox43.Items.Add(i); // adds the value of i to lixtbox
+                    counter++; // adds 1 to counter every time the if statement is true
+                }
+            }
+            labelResult.Text = $"How many times did [ x % 43 = 0 ]:\n{counter}"; // using the counter we can see how many times above if statement is true
+        }
+    }
+}
